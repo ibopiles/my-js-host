82f7300dfdb77f55245d4bb3d0acbc635a48959d
ibopiles <warpsiwa13@gmail.com>
2025-07-17 14:21:03 +0700

    Update nativebannerckzink.js

--- nativebannerckzink.js
+++ nativebannerckzink.js
@@ -1,2 +1,13 @@
-<script async="async" data-cfasync="false" src="//pl15524948.profitableratecpm.com/4a3b09ada51a3f2d39ed2d50ef30b2f4/invoke.js"></script>
-<div id="container-4a3b09ada51a3f2d39ed2d50ef30b2f4"></div>
+// profitablerate-ad.js
+
+(function () {
+  const script = document.createElement('script');
+  script.async = true;
+  script.setAttribute('data-cfasync', 'false');
+  script.src = "//pl15524948.profitableratecpm.com/4a3b09ada51a3f2d39ed2d50ef30b2f4/invoke.js";
+  document.body.appendChild(script);
+
+  const adDiv = document.createElement('div');
+  adDiv.id = "container-4a3b09ada51a3f2d39ed2d50ef30b2f4";
+  document.body.appendChild(adDiv);
+})();
