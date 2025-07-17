8dcc155f18361d8a39245dd492ec48c042320cd0
ibopiles <warpsiwa13@gmail.com>
2025-07-17 14:19:47 +0700

    Update bannerckzink.js

--- bannerckzink.js
+++ bannerckzink.js
@@ -1,10 +1,17 @@
-<script type="text/javascript">
-	atOptions = {
-		'key' : '9c7a0ba8968020f1c3a24f8dda4882cc',
-		'format' : 'iframe',
-		'height' : 250,
-		'width' : 300,
-		'params' : {}
-	};
-</script>
-<script type="text/javascript" src="//www.highperformanceformat.com/9c7a0ba8968020f1c3a24f8dda4882cc/invoke.js"></script>
+// ads-banner.js
+
+window.atOptions = {
+  'key': '9c7a0ba8968020f1c3a24f8dda4882cc',
+  'format': 'iframe',
+  'height': 250,
+  'width': 300,
+  'params': {}
+};
+
+// Buat elemen script dan sisipkan invoke.js ke halaman
+(function () {
+  const script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = '//www.highperformanceformat.com/9c7a0ba8968020f1c3a24f8dda4882cc/invoke.js';
+  document.body.appendChild(script);
+})();
